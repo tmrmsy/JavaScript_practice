21c3dbbc2f8a3d13336d8cae490fcb44a9dd3137
tmrmsy <98762396+tmrmsy@users.noreply.github.com>
2022-08-27 23:56:18 +0900

    jQueryCSS操作

--- jQuery-Lesson/app.js
+++ jQuery-Lesson/app.js
@@ -1,6 +1,9 @@
-//記述ルール
-//$(セレクタ).メソッド(パラメータ);
-$(document).ready(function(){
-  //jQueryプログラムの内容を記述
-  $('body').html('<h1>Hello jQuery!!</h1>');
+$(function(){
+  $('.box1').slideDown(function(){
+    $('.box1').css({
+      'background-color':'#0000FF',
+      'height':'100px',
+      'weight':'200px'
+    }).slideUp();
+  });
 });
